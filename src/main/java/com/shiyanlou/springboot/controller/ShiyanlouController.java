b8766a3bc378ba1eaabef39f076c783468495866
Jesse3692 <lcf.zhangxing@hotmail.com>
2024-02-19 23:23:39 +0800

    feat: 03-01 访问配置文件中的自定义属性

--- src/main/java/com/shiyanlou/springboot/controller/ShiyanlouController.java
+++ src/main/java/com/shiyanlou/springboot/controller/ShiyanlouController.java
@@ -1,13 +1,17 @@
 package com.shiyanlou.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 // RestController 相当于同时使用 @Controller 和 @ResponseBody 注解
 public class ShiyanlouController {
+    // 使用@Value注解注入属性值
+    @Value("${shiyanlou.springboot}")
+    private String shiyanlou;
     @RequestMapping("shiyanlou")
     public String shiyanlou() {
-        return "Hello Shiyanlou";
+        return shiyanlou;
     }
 }
