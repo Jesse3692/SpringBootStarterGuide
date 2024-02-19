b0c934dfbdfa0cc2fa0b4dbe845d76642fef9475
Jesse3692 <lcf.zhangxing@hotmail.com>
2024-02-19 23:32:33 +0800

    feat: 03-02 自定义配置文件

--- src/main/java/com/shiyanlou/springboot/controller/ShiyanlouController.java
+++ src/main/java/com/shiyanlou/springboot/controller/ShiyanlouController.java
@@ -1,15 +1,20 @@
 package com.shiyanlou.springboot.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 // RestController 相当于同时使用 @Controller 和 @ResponseBody 注解
+// 加载classpath目录下的shiyanlou.properties文件
+@PropertySource(value = "classpath:shiyanlou.properties")
 public class ShiyanlouController {
+
     // 使用@Value注解注入属性值
-    @Value("${shiyanlou.springboot}")
+    @Value("${shiyanlou.test}")
     private String shiyanlou;
+
     @RequestMapping("shiyanlou")
     public String shiyanlou() {
         return shiyanlou;
