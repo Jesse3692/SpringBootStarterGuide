65b830f65122a006764c5a0579eb168626f2727c
Jesse3692 <lcf.zhangxing@hotmail.com>
2024-02-20 11:49:43 +0800

    feat: 04-01 视图使用

--- src/main/java/com/shiyanlou/springboot/SpringbootApplication.java
+++ src/main/java/com/shiyanlou/springboot/SpringbootApplication.java
@@ -2,12 +2,9 @@ package com.shiyanlou.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 
 // @SpringBootApplication 注解相当于同时使用 @EnableAutoConfiguration、@ComponentScan、@Configurations 三个注解
 // @EnableAutoConfiguration 用于打开 Spring Boot 自动配置，而其余注解为 Spring 注解，这里不再赘述
-// 通过 @ImportResource 加载 xml 配置文件
-@ImportResource(value = "classpath:config.xml")
 @SpringBootApplication
 public class SpringbootApplication {
 
